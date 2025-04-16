f1274e279b3896a8b699ddf37d4ce3b0fe4943a8
quin1stein <sepriothofficial@gmail.com>
2025-04-16 16:09:25 +0800

    git init

---
+++ nvim/init.lua
@@ -0,0 +1,16 @@
+
+-- Set basic options
+vim.o.number = true                  -- show line numbers
+vim.o.clipboard = "unnamedplus"
+
+
+
+
+-- Change key commands
+vim.g.mapleader = " "                   -- Keyleader to <space>
+vim.keymap.set('v', "<C-c>", '"+y')     -- Copy selected lines in VISUAL MODE
+vim.keymap.set('n', "<C-v>", '"+p')     -- Paste copied lines in NORMAL MODE
+vim.keymap.set('i', "<C-v>", '<C-r>+')
+
+vim.keymap.set("n", "go", "m`o<Esc>``", { desc = "New line below, stay in normal mode" })
+vim.keymap.set("n", "gO", "m`O<Esc>``", { desc = "New line above, stay in normal mode" })
