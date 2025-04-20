3d4443463931171bfbbf2dacc04ff43f0982d988
Quin <147179328+quin1stein@users.noreply.github.com>
2025-04-20 18:13:58 +0800

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,6 @@ vim.o.clipboard = "unnamedplus"
 vim.g.mapleader = " "                   -- Keyleader to <space>
 vim.keymap.set('v', "<C-c>", '"+y')     -- Copy selected lines in VISUAL MODE
 vim.keymap.set('n', "<C-v>", '"+p')     -- Paste copied lines in NORMAL MODE
-vim.keymap.set('i', "<C-v>", '<C-r>+')
 
 vim.keymap.set("n", "go", "m`o<Esc>``", { desc = "New line below, stay in normal mode" })
 vim.keymap.set("n", "gO", "m`O<Esc>``", { desc = "New line above, stay in normal mode" })
